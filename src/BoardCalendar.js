4b1014184ac9fd3a717b7f934d0b54aa48a3e786
Alex Chu <alex.chu@connexta.com>
2019-07-01 14:06:37 -0700

    Added Calendar framework based on React-Big-Calendar

---
+++ src/BoardCalendar.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment, { calendarFormat } from "moment";
+
+import '!style-loader!css-loader!./BoardCalendar.css';
+import events from './events.json';
+
+const localizer = momentLocalizer(moment);
+
+const CalendarStyle = styled.div`
+display: flex;
+`;
+
+
+function updateEvents(eventData){
+  eventData = eventData.map(event => ({"title": event.subject, "start": event.start, "end": event.end}));
+  for (var i = 0; i < eventData.length; i++){
+    eventData[i].start = localizeTime(eventData[i].start.dateTime, eventData[i].start.timeZone);
+    eventData[i].end = localizeTime(eventData[i].end.dateTime, eventData[i].end.timeZone);
+  }
+  console.log(eventData);
+  return eventData;
+}
+
+function localizeTime(time, timezone){
+  return new Date(time.split("T") + " " + timezone);
+}
+
+//date: inclusive:exclusive. month needs to be -1
+class BoardCalendar extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: updateEvents(events.value)
+    };
+  }
+
+  render() {
+    updateEvents(events.value);
+    return (
+      <div>
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          style={{ height: "55vh" }}
+        />
+      </div>
+    );
+  }
+}
+
+export default BoardCalendar;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import logo from "../resources/logo-white.png";
 import ClockFull from "./clock.js";
 import BuildStatus from "./BuildStatus";
 import SlackComponent from "./SlackComponent";
+import BoardCalendar from "./BoardCalendar";
 
 const Banner = styled.nav`
   background: ${CX_DARK_BLUE};
@@ -85,6 +86,7 @@ ReactDOM.render(
         <LeftBox>
           {/* Left box content */}
           <BuildStatus />
+          <BoardCalendar/>
         </LeftBox>
         <RightBox>
           {/* Right box content */}
